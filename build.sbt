d1d440da1778001cfd8c632fd94dd72b10c4bd0a
KAWACHI Takashi <tkawachi@gmail.com>
2014-07-23 23:48:29 +0900

    impl

--- build.sbt
+++ build.sbt
@@ -5,3 +5,7 @@ organization := "com.github.tkawachi"
 name := "hocon2json"
 
 version := "0.1.0"
+
+libraryDependencies ++= Seq(
+  "com.typesafe" % "config" % "1.2.1"
+)
--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,5 +1,9 @@
 package com.github.tkawachi
 
+import java.io.File
+
+import com.typesafe.config.{ConfigFactory, ConfigRenderOptions}
+
 /** The launched conscript entry point */
 class App extends xsbti.AppMain {
   def run(config: xsbti.AppConfiguration) = {
@@ -7,17 +11,40 @@ class App extends xsbti.AppMain {
   }
 }
 
+/**
+ * Command to convert HOCON to JSON.
+ */
 object App {
-  /** Shared by the launched version and the runnable version,
-   * returns the process status code */
-  def run(args: Array[String]): Int = {
-    println("Hello World: " + args.mkString(" "))
-    0
+  private def usage = "Usage: hocon2json [-p] srcFile"
+
+  private def toJSON(file: File, pretty: Boolean): String = {
+    val renderOption =
+      if (pretty) ConfigRenderOptions.concise().setFormatted(true)
+      else ConfigRenderOptions.concise()
+    val conf = ConfigFactory.parseFile(file)
+    conf.root().render(renderOption)
   }
+
+  /** Shared by the launched version and the runnable version,
+    * returns the process status code */
+  def run(args: Array[String]): Int = {
+    args match {
+      case Array("-p", srcFileName) =>
+        println(toJSON(new File(srcFileName), true))
+        0
+      case Array(srcFileName) =>
+        println(toJSON(new File(srcFileName), false))
+        0
+      case _ =>
+        println(usage)
+        1
+    }
+  }
+
   /** Standard runnable class entrypoint */
   def main(args: Array[String]) {
     System.exit(run(args))
   }
 }
 
-case class Exit(val code: Int) extends xsbti.Exit
+case class Exit(code: Int) extends xsbti.Exit
