1c0db2d0e08bf9511bb3a8b13d9e72a5e0dd20fc
Takashi Kawachi <tkawachi@gmail.com>
2014-07-24 11:01:44 +0900

    handle path

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,4 +1,4 @@
-package com.github.tkawachi
+package com.github.tkawachi.hocon2json
 
 import java.io.File
 
@@ -15,25 +15,36 @@ class App extends xsbti.AppMain {
  * Command to convert HOCON to JSON.
  */
 object App {
-  private def usage = "Usage: hocon2json [-p] srcFile"
+  final val LS = System.lineSeparator
+  private def usage = s"Usage: hocon2json [-p] srcFile [path]"
 
-  private def toJSON(file: File, pretty: Boolean): String = {
+  private def toJSON(file: File, pretty: Boolean, rootPath: Option[String]): String = {
     val renderOption =
       if (pretty) ConfigRenderOptions.concise().setFormatted(true)
       else ConfigRenderOptions.concise()
     val conf = ConfigFactory.parseFile(file).resolve()
-    conf.root().render(renderOption)
+    val rootConf = rootPath match {
+      case Some(path) => conf.getConfig(path)
+      case None => conf
+    }
+    rootConf.root().render(renderOption)
   }
 
   /** Shared by the launched version and the runnable version,
     * returns the process status code */
   def run(args: Array[String]): Int = {
     args match {
+      case Array("-p", srcFileName, path) =>
+        println(toJSON(new File(srcFileName), true, Option(path)))
+        0
       case Array("-p", srcFileName) =>
-        println(toJSON(new File(srcFileName), true))
+        println(toJSON(new File(srcFileName), true, None))
+        0
+      case Array(srcFileName, path) =>
+        println(toJSON(new File(srcFileName), false, Option(path)))
         0
       case Array(srcFileName) =>
-        println(toJSON(new File(srcFileName), false))
+        println(toJSON(new File(srcFileName), false, None))
         0
       case _ =>
         println(usage)
